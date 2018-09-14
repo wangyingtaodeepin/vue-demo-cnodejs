fc28511d45ced6a4bbe61db54df07e53f076e21f
wangyingtao <wangyingtao@whbaihongsoft.com>
2018-09-14 15:51:30 +0800

    显示列表时间和标题

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <Header></Header>
-    <div class="article_list">
+    <div class="article_listtest">
       <ul>
-        <li></li>
+        <li v-for="i in list" v-bind:key="i.create_at">
+          <time v-text="$utils.goodTime(i.create_at)"></time>
+          <router-link :to="'/content/' + i.id">
+            {{ i.title }}
+          </router-link>
+        </li>
       </ul>
     </div>
     <Footer></Footer>
@@ -25,12 +30,12 @@ export default {
   methods: {
     getData () {
       this.$api.get('topics', null, r => {
-        console.log(r)
+        this.list = r.data
       })
     }
   }
 }
 </script>
 <style>
-  .article_list {margin: auto;}
+  .article_listtest {margin: auto;}
 </style>
