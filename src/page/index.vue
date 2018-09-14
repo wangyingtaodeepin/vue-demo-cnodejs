1ff38beb7179607eeb9fd323f662e5ad9d061651
wangyingtao <wangyingtao@whbaihongsoft.com>
2018-09-14 14:45:41 +0800

    修改index.vue导入header和footer组件

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>index page</div>
+  <div>
+    <Header></Header>
+    <div class="article_list">
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
+import Header from '../components/header.vue'
+import Footer from '../components/footer.vue'
 export default {
+  components: { Header, Footer },
+  data () {
+    return {
+      list: []
+    }
+  },
   created () {
-    this.$api.get('topics', null, r => {
-      console.log(r)
-    })
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$api.get('topics', null, r => {
+        console.log(r)
+      })
+    }
   }
 }
 </script>
+<style>
+  .article_list {margin: auto;}
+</style>
